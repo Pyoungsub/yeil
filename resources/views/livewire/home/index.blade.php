79684f20a4d1083e523f4c875d1bfcc7b7a89108
Pyoungsub <ps.shim87@gmail.com>
2024-08-29 21:06:57 +0900

    instagram

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -2,6 +2,7 @@
     <x-youtube-banner source="{{ asset('storage/video/7cf4958d5002916a5141c3b18de475d8.mp4') }}"></x-youtube-banner>
     <x-courses />
     <x-facilities />
+    <x-instagram />
     <div class="mt-12 max-w-xl mx-auto grid gap-12">
         <img class="aspect-video w-full flex items-center justify-center"  src="{{asset('storage/schedules/mNvXDdVAhpyDQWQSRBm3Ekt6xKBopMye5NqqKiut.png')}}" />
         <div class="aspect-video w-full flex items-center justify-center">
