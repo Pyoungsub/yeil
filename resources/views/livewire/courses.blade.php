cb4367fdfd52bfc87a15ceb9589cc27726f9bf23
Pyoungsub <ps.shim87@gmail.com>
2026-04-06 09:58:10 +0900

    add video on main page

--- resources/views/livewire/courses.blade.php
+++ resources/views/livewire/courses.blade.php
@@ -92,7 +92,6 @@
                         @endif
                     </div>
                 </div>
-                
             </div>
             <livewire:lessons.videos :lesson="$lesson" />
         @endforeach 
